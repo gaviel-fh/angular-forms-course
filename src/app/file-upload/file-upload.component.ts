0825069475f2a425d906a90bd31967b6a877cc7d
David Raese <david.raese@protonmail.com>
2021-11-03 19:04:34 +0100

    42. Implementing a File Upload Progress Indicator

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input } from "@angular/core";
 import { HttpClient, HttpEventType } from "@angular/common/http";
-import { catchError, finalize } from "rxjs/operators";
+import {
+  catchError,
+  debounceTime,
+  finalize,
+  throttleTime,
+} from "rxjs/operators";
 import {
   AbstractControl,
   ControlValueAccessor,
@@ -20,6 +25,7 @@ export class FileUploadComponent {
   public requiredFileType: string;
   public fileName: string = "";
   public fileUploadError: boolean = false;
+  public uploadProgress: number;
 
   constructor(private http: HttpClient) {}
 
@@ -32,14 +38,27 @@ export class FileUploadComponent {
       formData.append("thumbnail", file);
       this.fileUploadError = false;
       this.http
-        .post("/api/thumbnail-upload", formData)
+        .post("/api/thumbnail-upload", formData, {
+          reportProgress: true,
+          observe: "events",
+        })
         .pipe(
+          throttleTime(100),
           catchError((error) => {
             this.fileUploadError = true;
             return of(error);
+          }),
+          finalize(() => {
+            this.uploadProgress = null;
           })
         )
-        .subscribe();
+        .subscribe((event) => {
+          if (event.type == HttpEventType.UploadProgress) {
+            this.uploadProgress = Math.round(
+              100 * (event.loaded / event.total)
+            );
+          }
+        });
     }
   }
 }

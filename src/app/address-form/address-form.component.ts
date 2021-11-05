a671f31db3bd0830b2f85916ec7213d98377dd98
David Raese <david.raese@protonmail.com>
2021-11-05 12:22:48 +0100

    48. How to Use ControlValueAccessor to implement a Nested Form

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -1,36 +1,68 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import {
   AbstractControl,
   ControlValueAccessor,
-  FormBuilder, FormGroup,
+  FormBuilder,
+  FormGroup,
   NG_VALIDATORS,
   NG_VALUE_ACCESSOR,
   Validator,
-  Validators
-} from '@angular/forms';
-import {noop, Subscription} from 'rxjs';
+  Validators,
+} from "@angular/forms";
+import { noop, Subscription } from "rxjs";
 
 @Component({
-  selector: 'address-form',
-  templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss']
+  selector: "address-form",
+  templateUrl: "./address-form.component.html",
+  styleUrls: ["./address-form.component.scss"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: AddressFormComponent,
+    },
+  ],
 })
-export class AddressFormComponent {
+export class AddressFormComponent implements ControlValueAccessor, OnDestroy {
+  @Input()
+  legend: string;
+  public onTouched: Function = () => {};
+  public onChangeSubscription: Subscription;
 
-    @Input()
-    legend:string;
+  form: FormGroup = this.fb.group({
+    addressLine1: [null, [Validators.required]],
+    addressLine2: [null, [Validators.required]],
+    zipCode: [null, [Validators.required]],
+    city: [null, [Validators.required]],
+  });
 
-    form: FormGroup = this.fb.group({
-        addressLine1: [null, [Validators.required]],
-        addressLine2: [null, [Validators.required]],
-        zipCode: [null, [Validators.required]],
-        city: [null, [Validators.required]]
-    });
+  constructor(private fb: FormBuilder) {}
 
-    constructor(private fb: FormBuilder) {
+  writeValue(value: any) {
+    if (value) {
+      this.form.setValue(value);
     }
+  }
 
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChangeSubscription = this.form.valueChanges.subscribe((formValues) =>
+      onChange(formValues)
+    );
+  }
+
+  setDisabledState(disabled: boolean) {
+    if (disabled) {
+      this.form.disable();
+    } else {
+      this.form.enable();
+    }
+  }
+
+  ngOnDestroy() {
+    this.onChangeSubscription.unsubscribe();
+  }
 }
-
-
-

58a1d5f6df64027190d564b0615384c1c9aa7696
David Raese <david.raese@protonmail.com>
2021-11-08 19:22:26 +0100

    49. New Section - Setting Up Step 3 of our Multi-step Form

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,13 +1,12 @@
-import {Component} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component } from "@angular/core";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'create-course-step-3',
-  templateUrl: 'create-course-step-3.component.html',
-  styleUrls: ['create-course-step-3.component.scss']
+  selector: "create-course-step-3",
+  templateUrl: "create-course-step-3.component.html",
+  styleUrls: ["create-course-step-3.component.scss"],
 })
 export class CreateCourseStep3Component {
-
-
+  constructor(private fb: FormBuilder) {}
+  form: FormGroup = this.fb.group({});
 }
